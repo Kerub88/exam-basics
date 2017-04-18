474057f81cebd1e782ab54da1ea9b5dd3dbb27eb
Simon_NT <kacsa1984@gmail.com>
2017-04-18 12:32:42 +0200

    feat(black_jack): get_info from the deck and count the different cards in the deck method. Need more working on it. This is the current status of the two method.

--- blackjack/black_jack.py
+++ blackjack/black_jack.py
@@ -24,13 +24,13 @@
 import random
 
 class Card(object):
-    colors = ["Clubs", "Diamonds", "Hearts", "Pikes"]
+    colors = ["Clubs", "Diamonds", "Hearts", "Spades"]
     value = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
 
     def __init__(self, color, value):
         self.color = color
         self.value = value
-        self.card = {self.color, self.value}
+        self.card = {self.color : self.value}
 
     def card_info(self):
         return str(self.card[0]) + " " + str(self.card[1])
@@ -57,3 +57,17 @@ class Deck(Card):
             card = Card(card_color, card_value)
             self.deck_cards.append(card)
             self.number_of_cards -= 1
+
+    def count_cards(self, color_or_value):
+        number = 0
+        for i in self.deck_cards:
+            for k, v in i:
+                if k == color_or_value:
+                    number += 1
+                elif v == color_or_value:
+                    number += 1
+        return number
+
+
+    def get_info(self):
+        return str(len(self.deck_cards)) + " cards - " + str(self.count_cards("Clubs")) + " Clubs, " + str(self.count_cards("Diamonds")) + " Diamonds, " + str(self.count_cards("Hearts")) + " Hearts, " + str(self.count_cards("Spades")) + " Spades"
