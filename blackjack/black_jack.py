dbbfc9bc174a8ead423baa0d1e492c92ee416fa3
Simon_NT <kacsa1984@gmail.com>
2017-04-18 12:08:51 +0200

    feat(black_jack): deck class constructor creat random cards for the deck

--- blackjack/black_jack.py
+++ blackjack/black_jack.py
@@ -21,6 +21,8 @@
 # Queen Spades
 # 11 cards - 3 Clubs, 3 Diamonds, 3 Hearts, 2 Spades
 
+import random
+
 class Card(object):
     colors = ["Clubs", "Diamonds", "Hearts", "Pikes"]
     value = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
@@ -28,11 +30,38 @@ class Card(object):
     def __init__(self, color, value):
         self.color = color
         self.value = value
-        self.card = [self.color, self.value]
+        self.card = {self.color, self.value}
 
     def card_info(self):
         return str(self.card[0]) + " " + str(self.card[1])
 
-card1 = Card("Clubs", "2")
+class Deck(Card):
 
-print(card1.card_info())
+    colors = ["Clubs", "Diamonds", "Hearts", "Pikes"]
+    value = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+
+    def __init__(self, number_of_cards = 4):
+        self.number_of_cards = number_of_cards
+        self.deck_cards = []
+
+        while self.number_of_cards > 0:
+            random_colors = random.randrange(0, 3)
+            random_value = random.randrange(0, 13)
+            card_color = ""
+            card_value = ""
+            for i in range(len(Card.colors)):
+                if i == random_colors:
+                    card_color += Card.colors[i]
+                    break
+            for v in range(len(Card.value)):
+                if v == random_value:
+                    card_value += Card.value[v]
+                    break
+            card = Card(card_color, card_value)
+            self.deck_cards.append(card)
+            self.number_of_cards -= 1
+
+card1 = Card("Clubs", "2")
+deck1 = Deck(8)
+print(deck1.deck_cards)
+# print(Card.colors)
