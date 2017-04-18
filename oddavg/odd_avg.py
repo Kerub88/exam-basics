d1dc037b862c65d340ad02e4e6e68b639c6d1952
Simon_NT <kacsa1984@gmail.com>
2017-04-18 09:38:47 +0200

    feat(odd_avarage): a funktion which sum the avarage of the odd numbers from a list of numbers

--- oddavg/odd_avg.py
+++ oddavg/odd_avg.py
@@ -1,3 +1,16 @@
 # Create a function called `odd_average` that takes a list of numbers as parameter
 # and returns the average value of the odd numbers in the list
 # Create basic unit tests for it with at least 3 different test cases
+
+numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def odd_average(list_of_numbers):
+    odd_numbers = 0
+    len_of_odd = 0
+    for i in list_of_numbers:
+        if i % 2 != 0:
+            odd_numbers += i
+            len_of_odd += 1
+    return odd_numbers // len_of_odd
+
+print(odd_average(numbers))
