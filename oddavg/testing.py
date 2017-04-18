10fecd3f5d0d8536addd5a69439fd6df9c2e6892
Simon_NT <kacsa1984@gmail.com>
2017-04-18 10:22:50 +0200

    test(oddavg): all 3 test is done

--- oddavg/testing.py
+++ oddavg/testing.py
@@ -6,7 +6,12 @@ class Test_Odd_Av(unittest.TestCase):
         test_list = [1, 2, 3, 4, 5, "a", "b", "c"]
         self.assertEqual(odd_avg.odd_average(test_list), 3)
 
+    def test_odd_av_no_list(self):
+        self.assertEqual(odd_avg.odd_average(1, 2, 3), 2)
 
+    def test_odd_av_not_just_numbers(self):
+        test_list = [2 + 3, 2 * 2, 5 - 1]
+        self.assertEqual(odd_avg.odd_average(test_list), 5) 
 
 if __name__ == "__main__":
     unittest.main()
