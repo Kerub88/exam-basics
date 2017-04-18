193627ad1bfb91a2f81dbbbf2fda40fa7538a4d2
Simon_NT <kacsa1984@gmail.com>
2017-04-18 11:03:21 +0200

    feat(copy): a funktion which copy the data from one file and paste to another

--- copy/copy.py
+++ copy/copy.py
@@ -8,12 +8,20 @@
 # Read all contents from it and write it to the destination
 
 def copy(source = 0, destination = 0):
+    source = source
+    destination = destination
     if source == 0 and destination == 0:
-        print("copy [source] [destination]")
+        return print("copy [source] [destination]")
     if destination == 0:
-        print("No destination provided")
+        return print("No destination provided")
+    fr = open(source, "r")
+    data = fr.read()
+    fr.close()
+    fw = open(destination, "w")
+    fw.write(data)
+    fw.close()
 
-    
+copy("/Volumes/Kaleb/GreenFox/exam-basics/copy/test1.txt", "/Volumes/Kaleb/GreenFox/exam-basics/copy/test2.txt")
 
-
-copy()
+# 1. The exercise info provide vague information about what kind of funktion should I have to write. Its working now, but Im not sure this was the expected result.
+# 2. I used absolute path for testing the funktion, because my txt editor (Atom) cant see files just by names. Anoying.
